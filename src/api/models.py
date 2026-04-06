3dd99d8dd21b80f619a3ee4bfd52bea404f717d6
Ruben Alba Gonzalez <39350323+Ruben-Alba-Gonzalez@users.noreply.github.com>
2026-04-06 17:43:02 +0000

    GDV-2: Added model User in the backend

--- src/api/models.py
+++ src/api/models.py
@@ -6,14 +6,17 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(20), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    
 
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
